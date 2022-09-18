267ad9c8efbd6fd535688c5c9bfa2743e5fd92ba
HarishkumarCSK <99026121+HarishkumarCSK@users.noreply.github.com>
2022-09-18 20:04:07 +0530

    Update JenkinsProject.java

--- src/org/jenkins/JenkinsProject.java
+++ src/org/jenkins/JenkinsProject.java
@@ -2,7 +2,7 @@ package org.jenkins;
 
 public class JenkinsProject {
 public static void main(String[] args) {
-	System.out.println("Hari");
+	System.out.println("Harish");
 	System.out.println("kumar");
 	System.out.println("Harishkumar");
 }
